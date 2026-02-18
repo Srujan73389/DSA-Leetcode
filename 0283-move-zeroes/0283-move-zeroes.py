203c2e3e7c64c9eb07b24ff3991109b88cc9fc62
SRUJAN G M <gms73389@gmail.com>
2026-02-18 23:14:32 +0530

    Time: 9 ms (34.51%), Space: 13.7 MB (16.49%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        l=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[l]=nums[l],nums[i]
+                l+=1
+
+        
+        
