5b9b73d4b0e2cbadf4727a4eba1176c59824d2b9
SRUJAN G M <gms73389@gmail.com>
2026-02-20 20:53:41 +0530

    Time: 1 ms (63.63%), Space: 12.4 MB (56.32%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x):
+        L, R = 1, x
+
+        while L <= R:
+            M = (L+R) // 2
+            M_squared = M * M
+
+            if M_squared == x:
+                return M
+
+            elif M_squared < x:
+                L = M + 1
+
+            else:
+                R = M - 1
+
+        return R
