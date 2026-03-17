ad0e62a9688da553c802304182a63928ee8ae757
SRUJAN G M <gms73389@gmail.com>
2026-03-17 23:24:22 +0530

    Time: 8 ms (61.72%), Space: 14.6 MB (5.86%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        ans,count=0,0
+        for i in range(len(nums)):
+            if (count==0):
+                ans=nums[i]
+            if ans==nums[i]:
+                count+=1
+            else:
+                count-=1
+        return ans
+        
