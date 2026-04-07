91e5e66a3d3b266c7da10608894a21ef56ba7663
SRUJAN G M <gms73389@gmail.com>
2026-04-07 19:13:32 +0530

    Time: 19 ms (66.79%), Space: 12.4 MB (91.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        s_h={}
-        t_h={}
+        s_h,t_h={},{}
         for i in s:
             s_h[i]=s_h.get(i,0)+1
         for i in t:
             t_h[i]=t_h.get(i,0)+1
         return s_h==t_h
-
         
         
