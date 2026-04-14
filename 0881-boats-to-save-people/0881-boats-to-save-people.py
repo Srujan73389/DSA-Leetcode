d9dd1b24836776f6158f0e1b575c6f70a096a87c
SRUJAN G M <gms73389@gmail.com>
2026-04-14 12:05:09 +0530

    Time: 73 ms (28.09%), Space: 16.4 MB (91.53%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -6,11 +6,8 @@ class Solution(object):
         while start<=end:
             if people[start]+people[end]<=limit:
                 start+=1
-                end-=1
-                boat+=1
-            else:
-                end-=1
-                boat+=1
+            end-=1
+            boat+=1
             
         return boat
        
