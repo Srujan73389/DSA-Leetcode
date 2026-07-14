723d2fb94ac556c56d9aa4d1adfe4689a1133e89
SRUJAN G M <gms73389@gmail.com>
2026-07-14 21:27:27 +0530

    Time: 0 ms (100%), Space: 20.3 MB (64.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-                
+            diff =target-nums[i]
+            if diff in seen:
+                return [seen[diff],i]
+            else:
+                seen[nums[i]]=i
         
