9187b376613976d2c6bbcd9297d54278bc110e11
SRUJAN G M <gms73389@gmail.com>
2026-07-17 08:44:23 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,8 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         l,m,h=0,0,len(nums)-1
         while m<=h:
             if nums[m]==0:
@@ -8,9 +11,7 @@ class Solution(object):
                 m+=1
             elif nums[m]==1:
                 m+=1
-            else:
+            elif nums[m]==2:
                 nums[m],nums[h]=nums[h],nums[m]
                 h-=1
-
-        
         
