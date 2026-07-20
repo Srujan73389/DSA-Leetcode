1f81fa800276d564a6c793ba2c758f06f977b494
SRUJAN G M <gms73389@gmail.com>
2026-07-20 07:56:24 +0530

    Time: 24 ms (18.94%), Space: 32.4 MB (29.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        has=set()
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        ans=set()
         for i in nums:
-            if i in has:
+            if i in ans:
                 return True
-            has.add(i)
+            ans.add(i)
         return False
-        
         
