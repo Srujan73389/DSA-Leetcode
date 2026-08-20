f7f1127d5e937dca30f92b8128adb17c5d837e69
SRUJAN G M <gms73389@gmail.com>
2026-08-20 20:58:23 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.66%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        grp=set()
+        cnt={}
         for n in nums:
-            if n in grp:
-                grp.remove(n)
-            else:
-                grp.add(n)
-        return len(grp)==0
+            cnt[n]=cnt.get(n,0)+1
+        for i in cnt.values():
+            if i%2!=0:
+                return False
+        return True
         
