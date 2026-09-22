2d69708bd86aeca3fdeaa3fdc2b71de9f90270b7
SRUJAN G M <gms73389@gmail.com>
2026-09-22 11:43:04 +0530

    Time: 345 ms (79.45%), Space: 34.3 MB (31.51%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -1,14 +1,16 @@
 class Solution:
     def resultArray(self, nums: List[int], k: int) -> List[int]:
         res=[0]*k
-        prev=[0]*k
+        pre=[0]*k
         for num in nums:
             curr=[0]*k
             r=num%k
             curr[r]+=1
             for old in range(k):
-                curr[(old*r)%k]+=prev[old]
-            prev=curr
+                curr[(old*r)%k]+=pre[old]
+            pre=curr
             for i in range(k):
-                res[i]+=prev[i]
+                res[i]+=pre[i]
         return res
+
+        
